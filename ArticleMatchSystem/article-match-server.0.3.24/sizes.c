1263699c305c779bf71e5e7d82dbf6ad134a4f43
kouamano <amano.au1@gmail.con>
2014-09-20 00:22:27 +0900

    dd old

---
+++ ArticleMatchSystem/article-match-server.0.3.24/sizes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "data_op.h"
+#include "search_func.h"
+
+int main(){
+	printf("unsigned int :%d: Byte\n",sizeof(unsigned int));
+	printf("unsigned int :%d: Byte\n",sizeof(unsigned int));
+	printf("unsigned int * :%d: Byte\n",sizeof(unsigned int *));
+	printf("int :%d: Byte\n",sizeof(int));
+	printf("NGRAM_UNSIG_32 :%d: Byte\n",sizeof(NGRAM_UNSIG_32));
+}
